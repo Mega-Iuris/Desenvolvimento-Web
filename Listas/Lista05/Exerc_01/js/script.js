51b018066330e471d100c932dce3fde1deff490f
Mega-Iuris <iuris.p.medeiros@outlook.com>
2024-05-20 20:27:41 -0300

    Atualização lista04 e Lista 05

---
+++ Listas/Lista05/Exerc_01/js/script.js
@@ -0,0 +1,11 @@
+var nome
+var idade
+var empregoAtual
+var endereco
+
+nome = prompt("Digite seu nome: ");
+idade = prompt("informe sua idade: ");
+empregoAtual = prompt("Informe qual seu emprego atual: ");
+endereco = prompt("Digite seu endereço: ");
+
+alert(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é ${empregoAtual} \nE mora no endereço: ${endereco}`)
---
+++ Listas/Lista05/Exerc_02/js/ex02.js
@@ -0,0 +1,8 @@
+var nome = "Lorranny"
+var idade = 21
+var empregoAtual = "Enfermeira"
+var endereco = "Rua segunda, 100"
+
+
+
+console.log(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é ${empregoAtual} \nE mora no endereço: ${endereco}`)
